45bb81ba18c37eb3dc2a8dc6fee582542e00b954
kactusman <Rayanidrees4@gmail.com>
2026-09-15 23:01:40 +0500

    feat: add Google Tag Manager container GTM-P872836D and GSC tags

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,4 @@
 import { Bebas_Neue, Space_Grotesk, JetBrains_Mono } from "next/font/google";
-import Script from "next/script";
 import DaisyCat from "@/components/DaisyCat";
 import "./globals.css";
 
@@ -89,6 +88,9 @@ export const metadata = {
   alternates: {
     canonical: "/",
   },
+  verification: {
+    google: "PpjZqvWVsLkmOpkh_S7Y2A75EMpjXIfnb8yCc813M-8",
+  },
   openGraph: {
     title: "Rayan Idrees | Senior CMS Architect & Frontend Specialist",
     description:
@@ -146,6 +148,31 @@ export default function RootLayout({ children }) {
           property="og:image:secure_url"
           content="https://rayan-idrees.vercel.app/rayan-idrees-social-card-v2.png"
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P872836D');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
+        {/* Google Analytics (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-M8D0THYY51"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-M8D0THYY51');`,
+          }}
+        />
+        {/* End Google Analytics */}
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
@@ -155,20 +182,20 @@ export default function RootLayout({ children }) {
         className="font-body bg-onyx text-vanilla antialiased selection:bg-lime selection:text-onyx"
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P872836D"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll>
           {children}
         </SmoothScroll>
         <DaisyCat />
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-M8D0THYY51"
-          strategy="beforeInteractive"
-        />
-        <Script id="google-analytics" strategy="beforeInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-M8D0THYY51');`}
-        </Script>
       </body>
     </html>
   );
